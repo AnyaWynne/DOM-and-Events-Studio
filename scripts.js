f6835a1d29375d2b5c5c39c2aa1af3d89ca1c1ff
Anya Wynne <avkwynne@gmail.com>
2020-10-28 17:29:59 -0500

    DOM and Events Studio

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ takeOffButton.addEventListener("click", function(event){
     if (shuttleConfirm){
     flightStatus.innerHTML = "Shuttle in flight.";
     shuttleBackground.style.backgroundColor = "blue";
-    spaceShuttleHeight.innerHTML = "10,000";
+    spaceShuttleHeight.innerHTML =Number(spaceShuttleHeight.innerHTML) + 10000;
 
 }
 });
@@ -19,7 +19,6 @@ landButton.addEventListener("click", function(event){
         flightStatus.innerHTML = "The shuttle has landed.";
         shuttleBackground.style.backgroundColor = "green";
         spaceShuttleHeight.innerHTML = "0";
-        rocket.style.marginTop = "10px";
     
 });
 
@@ -30,29 +29,49 @@ missionAbort.addEventListener("click", function(event){
         flightStatus.innerHTML = "Mission Aborted.";
         shuttleBackground.style.backgroundColor = "green";
         spaceShuttleHeight.innerHTML = "0";
-        rocket.style.marginTop = "10px";
     }
 });
 
+let rocketImage = document.getElementById("rocket");
+rocketImage.style.position = "relative";
+rocketImage.style.top = "20px";
+rocketImage.style.left = "20px";
+rocketImage.style.visibility="visible";
+let vertPosString = rocketImage.style.top;
+let vertPos = Number(vertPosString.split("px")[0]);
+let horizonPosString = rocketImage.style.left;
+let horizPos = Number(horizonPosString.split("px")[0]);
+
+
 let leftButton = document.getElementById("leftButton");
 leftButton.addEventListener("click", function(event){
-    rocket.style.marginLeft = "-10px";
+    // rocket.style.marginLeft = "-10px";
+    if  (horizPos > -160) horizPos -= 10;
+        rocketImage.style.left = `${horizPos}px`;
 });
 
 let rightButton = document.getElementById("rightButton");
 rightButton.addEventListener("click", function(event){
-    rocket.style.marginLeft = "10px";
+    // rocket.style.marginLeft = "10px";
+    if (horizPos < 160) horizPos +=10;
+    rocketImage.style.left = `${horizPos}px`;
+
 });
 
 let upButton = document.getElementById("upButton");
 upButton.addEventListener("click", function(event){
-rocket.style.marginTop = "-10px";
+// rocket.style.marginTop = "-10px";
+if (vertPos > -10) vertPos -=10;
+rocketImage.style.top = `${vertPos}px`;
 spaceShuttleHeight.innerHTML = parseInt(spaceShuttleHeight.innerHTML) + 10000;
+
 });
 
 let downButton = document.getElementById("downButton");
 downButton.addEventListener("click", function(event){
-    rocket.style.marginTop = "10px";
+    if (vertPos < 250) vertPos +=10;
+    rocketImage.style.top = `${vertPos}px`;
+    // rocket.style.marginTop = "10px";
     spaceShuttleHeight.innerHTML = parseInt(spaceShuttleHeight.innerHTML)-10000;
 });
 
